ab79718a3a7ff4611110a2c805ffe82dcc434c86
Tonoy <tonoy@gmail.com>
2018-07-16 01:08:19 +0600

    Building and staff added
    Building and staff field are added in navigation manu

--- js/loader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#btn_home").click(function(){
-        $("#change").load("home.html");
-    });
-    $("#btn_student").click(function(){
-        $("#change").load("student.html");
-    });
-});
---
+++ js/main_loader.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#change").load("home.html");
+
+    /*Side navigation bar*/
+    $("#btn_home").click(
+        function () {
+            $("#change").load("home.html");
+        }
+    );
+    $("#btn_student").click(
+        function () {
+            $("#change").load("student.html");
+        }
+    );
+    $("#btn_building").click(
+        function () {
+            $("#change").load("building.html");
+        }
+    );
+    $("#btn_stuff").click(
+        function () {
+            $("#change").load("stuff.html");
+        }
+    );
+});
